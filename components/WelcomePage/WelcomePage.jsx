05c3ed7b92771a5eb38ec4f2fa1ea0c8ec070079
Agnieszka Józiak <agness.ciolek@gmail.com>
2018-12-15 18:57:18 +0100

    Welcome Page signs in with google via dedicated component

--- components/WelcomePage/WelcomePage.jsx
+++ components/WelcomePage/WelcomePage.jsx
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
-import bgmasthead from '../images/bgmasthead.jpg';
+import React from "react";
+import {Form, Col, FormGroup} from 'react-bootstrap';
+import GoogleButton from 'react-google-button';
 
+export default class WelcomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default class WelcomePage extends Component {
-
-  render()
-  {
-      let style = {textAlign: "center", color:"grey"};
-      let sectionStyle = {
-          width: "100%",
-          height: "250px",
-          backgroundPosition: "center",
-          backgroundSize: "auto",
-          backgroundRepeat: "no-repeat",
-          backgroundImage: `url(${bgmasthead})`
-      };
-    return (<section style={sectionStyle} >
-            <h1 style={style}>Create your own financial cosmos</h1>
-        </section>
-    );
-  }
+    btnHandler = (e) => {
+        e.preventDefault();
+        if (typeof this.props.loginHandler === 'function') {
+            this.props.loginHandler();
+        }
+    };
+    render(){
+        let sectionStyle = {
+            width: "100%",
+            height: "671px"
+        };
+        return (<section style={sectionStyle}>
+            <div style={{height: `${window.innerHeight/20}px`}}/>
+            <h1 className={"text-center display-1"}>Create your own financial cosmos</h1>
+            <div style={{height: `${0.5*window.innerHeight}px`}}/>
+            <Form horizontal>
+                <FormGroup>
+                    <Col smOffset={4} sm={4}>
+                        <GoogleButton onClick={this.btnHandler} />
+                    </Col>
+                </FormGroup>
+            </Form>;
+        </section>)
+    }
 }
+
