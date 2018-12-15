c60461975da183a4f3adf2026395a675fffbcaed
Agnieszka Józiak <agness.ciolek@gmail.com>
2018-12-15 18:55:17 +0100

    Refactor adjustments

--- components/Dashboard/BubblechartView.jsx
+++ components/Dashboard/BubblechartView.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import BubbleChart from '@weknow/react-bubble-chart-d3';
-import DateSpan from './DateSpan.jsx';
+import DateSpan from '../Header/DateSpan.jsx';
 import moment from "moment";
 
 export default class BubblechartView extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {view: 'expand'};
-
     }
 
     belongsToPeriodInQuestion(elem) {
@@ -17,13 +14,9 @@ export default class BubblechartView extends React.Component {
         return ( outflowAfterMinDate && outflowBeforeMaxDate);
     }
 
-    viewHandler = name => {
-        this.setState({view: name});
-    };
-
-    bubbleGenerator() {
-        if (this.state.view === 'compact') {
-            let filteredOutflows = this.props.outflows.filter(elem => this.belongsToPeriodInQuestion(elem));
+    render() {
+        let filteredOutflows = this.props.outflows.filter(elem => this.belongsToPeriodInQuestion(elem));
+        if (this.props.view === 'compact') {
             let labels = new Set(filteredOutflows.map(elem => elem.label));
             var data = [];
             for (let label of labels) {
@@ -40,30 +33,19 @@ export default class BubblechartView extends React.Component {
                 data.push({color: color, value: sum, label: label});
             }
         } else {
-            let filteredOutflows = this.props.outflows.filter(elem => this.belongsToPeriodInQuestion(elem));
             var data = filteredOutflows.map(elem => {
                 return {label: elem.name, value: elem.amount, color: elem.color}
             });
         }
-            return (data.length>0?
-                <BubbleChart
-                    data={data}
-                    graph={{zoom: 0.5, offsetX: 0.15, offsetY: 0.0}}
-                    width={900}
-                    height={500}
-                    showLegend={false}
-                    labelFont={{color: 'aliceblue', size: 11, weight: 'bold', family: 'Arial'}}
-                    valueFont={{color: 'aliceblue', size: 12, weight: 'lighter', family: 'Arial'}}
-                /> :
-                <><br/><span style={{color: "red", fontWeight: "bold"}}>
-                    No historical outlays in selected date span!</span></>)
-    }
 
-    render() {
-        return (<div>
-            <DateSpan minDate={this.props.minDate} maxDate={this.props.maxDate} handler={this.props.dateHandler}
-                      expander={true} viewHandler={this.viewHandler} view={this.state.view}/>
-            {this.bubbleGenerator()}
-        </div>);}
+        let toRender = data.length>0?<BubbleChart data={data} showLegend={false} width={900} height={500}
+                                                  graph={{zoom: 0.5, offsetX: 0.15, offsetY: 0.0}}
+                                                  labelFont={{color: 'aliceblue',
+                                                      size: 11, weight: 'bold', family: 'Arial'}}
+                                                  valueFont={{color: 'aliceblue',
+                                                      size: 12, weight: 'lighter', family: 'Arial'}}/> :
+            <><br/><span style={{color: "red", fontWeight: "bold"}}>
+                    No historical outlays in selected date span!</span></>;
+        return (toRender);
 }
-
+}
